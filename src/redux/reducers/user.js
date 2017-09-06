d39e8ff69f1e66c9fff9ecaa097ccd166170852f
Andy Parker <andyparker84@gmail.com>
2017-09-06 10:58:22 -0500

    fixed user.js

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,3 +1,26 @@
-const initialState = {
-    users: [{ id: 1, name: 'Steven'}]
+const reducer = (state = initialState, action) => {
+    switch ( action.type ) {
+        case ADD_USER:
+            return {
+                ...state,
+                users: [...state.users, action.payload]
+            }
+            break;
+        case DELETE_USER:
+            return {
+                ...state,
+                // action.payload should be an index
+                users: [...array.slice(0, action.payload), ...array.slice(action.payload + 1)]
+            }
+            break;
+        case UPDATE_USER:
+            return {
+                ...state,
+                users: state.users.map(val => val.id === action.payload.id ? { ...val, name: action.payload.name } : val)
+            }
+            break;
+        default:
+            return state;
+    }
+
 }
